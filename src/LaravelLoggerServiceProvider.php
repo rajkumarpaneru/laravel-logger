af7234354c933234586d2c5087f12e54f4d4184b
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-08 13:09:32 +0545

    specify directory to load migration files from

--- src/LaravelLoggerServiceProvider.php
+++ src/LaravelLoggerServiceProvider.php
@@ -1,13 +1,14 @@
 <?php
 
 namespace Raajkumarpaneru\LaravelLogger;
+
 use Illuminate\Support\ServiceProvider;
 
 class LaravelLoggerServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        //
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
     }
 
     public function boot()
