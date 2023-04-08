e4a4010f067c80246d8307389c95bc70824af9a6
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-08 12:41:29 +0545

    register namespace and service provider

---
+++ src/LaravelLoggerServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Raajkumarpaneru\LaravelLogger;
+use Illuminate\Support\ServiceProvider;
+
+class LaravelLoggerServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        //
+    }
+
+    public function boot()
+    {
+        //
+    }
+}
