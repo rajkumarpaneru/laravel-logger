47b9216571153492c12e6f4f5de892812834f673
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2023-04-18 21:01:05 +0545

    fix: table name in service provider

--- src/LaravelLoggerServiceProvider.php
+++ src/LaravelLoggerServiceProvider.php
@@ -8,7 +8,6 @@ class LaravelLoggerServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         $this->app['router']
             ->aliasMiddleware('enable-log', \Raajkumarpaneru\LaravelLogger\Middleware\LogQueryExecutionTimeMiddleware::class);
     }
@@ -16,10 +15,9 @@ class LaravelLoggerServiceProvider extends ServiceProvider
     public function boot()
     {
         if ($this->app->runningInConsole()) {
-            if (!class_exists('CreateLaravelQueryExecutionLogs')) {
+            if (!class_exists('CreateLaravelQueryExecutionLogsTable')) {
                 $this->publishes([
-                    __DIR__ . '/../database/migrations/create_laravel_query_execution_logs.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_laravel_query_execution_logs.php'),
-                    // you can add any number of migrations here
+                    dirname(__DIR__) . '/database/migrations/create_laravel_query_execution_logs_table.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_laravel_query_execution_logs_table.php'),
                 ], 'migrations');
             }
         }
